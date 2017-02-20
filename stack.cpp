4a5b3c508cc782e2dfd27773f217b1da5ea220ac
VaccineDevil <raving3475@mail.ru>
2017-02-20 23:59:57 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,123 +1,118 @@
+#include "stdafx.h"
 #include "stack.h"
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-node* first(int value)
+Stack::Stack()
 {
-	node *top = new node;//выделяем место в ячейке памяти под указатель на первый элемент
- 	top->value = value;  //записываем значение в информационное поле
- 	top->prev = 0;       //зануляем ссылку на предыдущий элемент
- 	return top;          // возвращаем адрес на первый элемент
-}
-void push(node **top, int value)
-{
-	node *pv = new node;//выделяем место в ячейке памяти 
-	pv->value = value;  //записываем значение в информационное поле
- 	pv->prev = *top;    //меняем указатель на предыдущий элемент
-    *top = pv;          //определение новой вершины
-}
-void pop(node **top)
-{
-	node *pv = *top; //выделяем место в ячейке памяти 
-    * top = (*top)->prev; // определение новой вершины
-	delete pv; //освобождение памяти 
+	top = NULL;
 }
 
-void print(node *top)
+Stack::~Stack()
 {
-    node *pv = top; //выделяем место в ячейке памяти
- 	while (pv != NULL) // если адрес вершины не указывает на 0(т.е. есть еще элементы) 
+	Clear();
+}
+
+void Stack::push(int a)		// Функция добавления элемента в вершину стека
+{ 
+	node* pv = new node();	//выделяем место в ячейке памяти 
+	pv->value = a;			//записываем значение в информационное поле
+	pv->next = top;			//меняем указатель вершины
+	top = pv;				//определение новой вершины
+}
+
+int Stack::pop()			// Функция удаления элемента из вершины стека, возвращает целочисленное значение
+{ 
+	node* pv = new node();	//выделяем место в ячейке памяти 
+	pv = top;				//присваиваем pv указатель на вершину стека
+	if (Stack::IsEmpty())	//Если стек пуст
 	{
-		cout << pv->value<<" "<< endl;
-		pv = pv->prev; // переход к следующему элементу
- 	}
-}
-bool isEmpty(node *top) //возращает false,если стек пуст и true, если стек заполнен хотя бы одним элементом
-{
-	if (top == NULL)
-		return false;
-	else
-                   return true;
-}
-void menu()
-{
-	int a, mn;
-    node *top = NULL;
-	setlocale(LC_ALL, "Russian");
-	do
-	{
-		cout << "Выберите пункт из меню:" << endl;
-		
-		cout << "1)Добавить элемент в стек" << endl;
-		cout << "2)Удалить элемент из стека" << endl;
-		cout << "3)Печать стека" << endl << endl;
-		cout << "Нажмите 0 для выхода из программы" << endl;
-		cin >> mn;
-		switch (mn)
-		{
-		case 1:
-		{
- 			cout << "Введите число в стек:" << endl;
-			cin >> a;
-			if (isEmpty(top) == 0)
- 			{
-				top = first(a);
- 				system("cls");
- 			}
- 			else
- 			{
-				push(&top, a);
-				system("cls");
-			}
-			break;
- 		}
- 		case 2:
- 		{
-			if (isEmpty(top) == NULL)
- 			{
-				system("cls");
- 				cout << "Элементов в стеке нет" << endl;
- 				cout << endl;
-				cout << endl;
-				cout << endl;
-			}
-			else
- 			{
-				pop(&top);
- 				system("cls");
- 				cout << endl;
- 				cout << endl;
- 				cout << endl;
- 			}
- 			break;
-
-
- 		}
-
-
- 		case 3:
- 		{
-			if (isEmpty(top) == 0)
-			{
- 				system("cls");
- 				cout << "Элементов в стеке нет" << endl;
-				cout << endl;
- 				cout << endl;
-				cout << endl;
-			}
- 			else
- 			{
- 				system("cls");
-				print(&top);
-				cout << endl;
-				cout << endl;
-				cout << endl;
-			}
- 			break;
-		}
+		return NULL;		//возвращаем 0, т.к. нечего удалять
 	}
-	if (mn<0 || mn>3)
-		system("cls");
-	} while (mn != 0);
+	else
+	{
+		int a;				//в переменную запишем значение удаленного элемента из вершины стека
+		a = pv->value;
+		pv = top; // возможно лишняя строчка
+		top = pv->next;		//меняем указатель на вершину стека
+		delete pv;			//удаляем элемент из стека
+		return a;			//возвращаем значение удалённого элемента стека
+	}
+}
+
+bool Stack::Search(int key)	//Функция поиска элемента из стека по ключу
+{
+	node *pv = new node();	//выделяем место в ячейке памяти 
+	pv = top;				//присваиваем pv указатель на вершину стека
+	while (pv)				// проходим по стеку пока вершина не ноль
+	{
+		if (pv->value == key)//если значение совпадает с ключом, возвращаем true, иначе false
+		{
+			return true;
+		}
+		pv = pv->next;		//меняем указатель, тем самым проходим по стеку
+	}
+	return false;
+}
+
+void Stack::Del() 
+{ 
+	node* pv = new node();	//выделяем место в ячейке памяти 
+	pv = top;				//присваиваем pv указатель на вершину стека
+	top = pv->next;			//меняем указатель вершины
+	delete pv;				//удаляем элемент
+}
+
+void Stack::RandPush(int amount, int range) 
+{
+	int random, i = 0;
+	node* elem = new node();
+	srand(time(NULL));
+	while (i != amount)
+	{
+		random = rand() % range;
+		push(random);
+		i++;
+	}
+}
+
+void Stack::Clear()	//Функция очистки стека
+{
+	while (top)		// пока указатель на вершину не ноль 
+	{
+		Del();		//удаляем элементы из стека
+	}
+}
+
+Stack::iterater Stack::getTop()	//Возвращает указатель на вершину стека
+{ 
+	return top;
+}
+
+Stack::iterater Stack::getNext(iterater a)	//Возвращает указатель на следующий элемент
+{ 
+	if (Stack::IsEmpty()) 
+	{
+		return NULL;
+	}
+	return a->next;
+}
+
+int Stack::getInf(iterater a)	//Возвращает значение элемента стека или 0, если стек пуст
+{
+	if (Stack::IsEmpty()) 
+	{
+		return NULL;
+	}
+	return a->value;
+}
+
+bool Stack::IsEmpty() //Возращает false, если стек пуст и true, если стек заполнен хотя бы одним элементом
+{ 
+	if (top)
+		return false;
+	else 
+		return true;
 }
