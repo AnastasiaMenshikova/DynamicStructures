1a4416419a25c7c986c7c51fc1d938a5ba2c7374
VaccineDevil <raving3475@mail.ru>
2017-02-26 18:20:08 +0300

    Update stack_main.cpp

--- stack_main.cpp
+++ stack_main.cpp
@@ -16,7 +16,7 @@ int main()
 	{
 		cout << "Выберите пункт меню:" << endl;
 		cout << "1)Добавить один элемент в стек" << endl;
-		cout << "2)Добавить рандомный(ые) элемент(ы)" << endl;
+		cout << "2)Заполнить стек рандомными значениями" << endl;
 		cout << "3)Удалить элемент из стека" << endl;
 		cout << "4)Очистить стек" << endl;
 		cout << "5)Поиск элемента в стеке по значению" << endl;
@@ -36,207 +36,198 @@ int main()
 		{
 
 
-			case 1:
+		case 1:
+		{
+			system("cls");
+			error = 1;
+			for (;;)
+			{
+				cout << "Введите одно значение для добавления в стек:" << endl;
+				if ((cin >> data) && (cin.good()))
+				{
+					break;
+				}
+				else
+				{
+					if (error == 1)
+					{
+						error = 0;
+						cout << "\n Некорректное значение. Введите число:" << endl;
+						system("pause");
+					}
+
+					cin.clear();
+					cin.ignore();
+					system("cls");
+				}
+			}
+			a.push(data);
+			system("cls");
+			cout << "Элемент был успешно добавлен." << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+			for (;;)
+			{
+				cout << "Введите количество элементов:" << endl;
+				if ((cin >> amount) && (cin.good()))
+				{
+				}
+				else
+				{
+					if (error == 1)
+					{
+						error = 0;
+						cout << "\n Некорректное значение.Введите число:" << endl;
+						system("pause");
+					}
+					cin.clear();
+					cin.ignore();
+					system("cls");
+					continue;
+				}
+
+				cout << "Введите максимальное значение:" << endl;
+				if ((amount > 0) && (cin >> range) && (cin.good()))
+				{
+					a.RandPush(amount, range);
+					system("cls");
+					cout << "Элементы были успешно добавлены." << endl;
+					system("pause");
+					system("cls");
+					break;
+					break;
+				}
+				else
+				{
+					if (error == 1)
+					{
+						error = 0;
+						cout << "\n Некорректное значение.Введите число:" << endl;
+						system("pause");
+					}
+					cin.clear();
+					cin.ignore();
+				}
+				system("cls");
+			}
+			system("cls");
+			break;
+		}
+
+		case 3:
+		{
+
+			system("cls");
+			if (a.IsEmpty())
 			{
 				system("cls");
-				error = 1;
-				for (;;)
-				{
-					cout << "Введите одно значение для добавления в стек:" << endl;
-					if ((cin >> data) && (cin.good()))
-					{
-						break;
-					}
-					else
-					{
-						if (error == 1)
-						{
-							error = 0;
-							cout << "\n Некорректное значение. Введите число:" << endl;
-							system("pause");
-						}
-
-						cin.clear();
-						cin.ignore();
-						system("cls");
-					}
-				}
-				a.push(data);
-				system("cls");
-				cout << "Элемент был успешно добавлен." << endl;
+				cout << "Невозможно удалить элемент, стек пуст. Добавьте элементы в стек." << endl;
 				system("pause");
-				system("cls");
-				break;
 			}
-			case 2:
+			else
+			{
+				a.Del();
+				cout << "Элемент был успешно удален." << endl;
+				system("pause");
+			}
+			system("cls");
+			break;
+		}
+
+		case 4:
+		{
+			system("cls");
+			if (a.IsEmpty())
+			{
+				cout << "Элементов нет." << endl;
+				system("pause");
+			}
+			else
+			{
+				a.Clear();
+				cout << "Стек был успешно очищен." << endl;
+				system("pause");
+			}
+			system("cls");
+			break;
+		}
+
+		case 5:
+		{
+			system("cls");
+			error = 1;
+			if (a.IsEmpty())
 			{
 				system("cls");
-				for (;;)
+				cout << "Стек пуст. Добавьте элементы для поиска." << endl;
+				system("pause");
+			}
+			else
+			{
+				cout << "Введите элемент для поиска:" << endl;
+				if ((cin >> data) && (cin.good()))
 				{
-					cout << "Введите количество элементов:" << endl;
-					if ((cin >> amount) && (cin.good()))
+					if (a.Search(data) == false)
 					{
+						cout << "Элемент не был найден." << endl;
 					}
 					else
 					{
-						if (error == 1)
-						{
-							error = 0;
-							cout << "\n Некорректное значение.Введите число:" << endl;
-							system("pause");
-						}
-						cin.clear();
-						cin.ignore();
-						system("cls");
-						continue;
+						cout << "Элемент " << data << " находится в стеке." << endl;
 					}
 
-					cout << "Введите максимальное значение:" << endl;
-					if ((amount > 0) && (cin >> range) && (cin.good()))
+				}
+				else
+				{
+					if (error == 1)
 					{
-						if (amount == 1)
-						{
-							a.RandPush(amount, range);
-							cout << "Элемент был успешно добавлен." << endl;
-							system("pause");
-							system("cls");
-							break;
-						}
-						else
-						{
-							a.RandPush(amount, range);
-							cout << "Элементы были успешно добавлены." << endl;
-							system("pause");
-							system("cls");
-							break;
-						}
+						error = 0;
+						cout << "\n Некорректное значение. Введите число:" << endl;
+						system("pause");
 					}
-					else
-					{
-						if (error == 1)
-						{
-							error = 0;
-							cout << "\n Некорректное значение.Введите число:" << endl;
-							system("pause");
-						}
-						cin.clear();
-						cin.ignore();
-					}
+
+					cin.clear();
+					cin.ignore();
 					system("cls");
-					}
-				system("cls");
-				break;
 				}
+				system("pause");
+			}
+			system("cls");
+			break;
+		}
 
-			case 3:
+
+		case 6:
+		{
+			system("cls");
+			iter = a.getTop();
+			if (a.IsEmpty())
 			{
+				cout << "В стеке нет элементов." << endl;
+				system("pause");
+			}
+			else
+			{
+				cout << "Элементы стека:" << endl;
+				while (iter)
+				{
+					cout << a.getInf(iter) << ' ';
+					iter = a.getNext(iter);
 
-				system("cls");
-				if (a.IsEmpty())
-				{
-					system("cls");
-					cout << "Невозможно удалить элемент, стек пуст. Добавьте элементы в стек." << endl;
-					system("pause");
 				}
-				else
-				{
-					a.Del();
-					cout << "Элемент был успешно удален." << endl;
-					system("pause");
-				}
-				system("cls");
-				break;
+				cout << endl;
+				system("pause");
 			}
 
-			case 4:
-			{
-				system("cls");
-				if (a.IsEmpty())
-				{
-					cout << "Элементов нет." << endl;
-					system("pause");
-				}
-				else
-				{
-					a.Clear();
-					cout << "Стек был успешно очищен." << endl;
-					system("pause");
-				}
-				system("cls");
-				break;
-			}
-			
-			case 5:
-			{
-				system("cls");
-				error = 1;
-				if (a.IsEmpty())
-				{
-					system("cls");
-					cout << "Стек пуст. Добавьте элементы для поиска." << endl;
-					system("pause");
-				}
-				else
-				{
-					cout << "Введите элемент для поиска:" << endl;
-					if ((cin >> data) && (cin.good()))
-					{
-						if (a.Search(data) == false)
-						{
-							cout << "Элемент не был найден." << endl;
-						}
-						else
-						{
-							cout << "Элемент " << data << " находится в стеке." << endl;
-						}
+			system("cls");
+			break;
 
-					}
-					else
-					{
-						if (error == 1)
-						{
-							error = 0;
-							cout << "\n Некорректное значение. Введите число:" << endl;
-							system("pause");
-						}
-
-						cin.clear();
-						cin.ignore();
-						system("cls");
-					}
-					system("pause");
-				}
-				system("cls");
-				break;
-			}
-			
-
-			case 6:
-			{
-				system("cls");
-				iter = a.getTop();
-				if (a.IsEmpty())
-				{
-					cout << "В стеке нет элементов." << endl;
-					system("pause");
-				}
-				else
-				{
-					cout << "Элементы стека:" << endl;
-					while (iter) 
-					{
-						cout << a.getInf(iter) << ' ';
-						iter = a.getNext(iter);
-						
-					}
-					cout << endl;
-					system("pause");
-				}
-		
-				system("cls");
-				break;
-
-			}
+		}
 
 
 		}
